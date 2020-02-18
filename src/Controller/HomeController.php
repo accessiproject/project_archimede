32354a2de5ca333b6417971d67ab43eb3f3da8c1
bustamante <kevin.bustamante@mail.novancia.fr>
2020-02-18 11:29:52 +0100

    creation HomeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home_default")
+     */
+    public function home_default()
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/accueil", name="home_index")
+     */
+    public function home_index()
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
